6c7f1337415c77443164eca5369a2b33a020609e
gabrielfernandessilva <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-05-25 02:01:15 -0300

    roteiro 1 finalizado

--- R01-03/src/test/java/sorting/test/StudentSortingTest.java
+++ R01-03/src/test/java/sorting/test/StudentSortingTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sorting.AbstractSorting;
-import sorting.simpleSorting.*;
+import sorting.variationsOfInsertionsort.RecursiveInsertionSort;
 
 public class StudentSortingTest {
 
@@ -32,7 +32,7 @@ public class StudentSortingTest {
 		populaVetorRepetido(new Integer[] { 4, 9, 3, 4, 0, 5, 1, 4 });
 		populaVetorIgual(new Integer[] { 6, 6, 6, 6, 6, 6 });
 
-		populaVetorSingleElem(new Integer[] {1,2,3,0});
+		populaVetorSingleElem(new Integer[] {67});
 		populaVetorDoubleElem(new Integer[] {-8, -15});
 		populaVetorStrings(new String[] {"Chorong", "Bomi", "Naeun", "Hayoung", 
 											"Eunji", "Namjoo"});
@@ -46,7 +46,7 @@ public class StudentSortingTest {
 	 * do aluno
 	 */
 	private void getImplementation() {
-		this.implementation = new BubbleSort<>();
+		this.implementation = new RecursiveInsertionSort<>();
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
@@ -133,7 +133,7 @@ public class StudentSortingTest {
 	@Test
 	public void testSort08() {
 		AbstractSorting<String> implementation2;
-		implementation2 = new BubbleSort<>();
+		implementation2 = new RecursiveInsertionSort<>();
 		String[] copy1 = Arrays.copyOf(vetorStrings, vetorStrings.length);
 		
 		System.out.println(Arrays.toString(vetorStrings));
@@ -147,7 +147,7 @@ public class StudentSortingTest {
 	@Test
 	public void testSort09() {
 		AbstractSorting<String> implementation2;
-		implementation2 = new BubbleSort<>();
+		implementation2 = new RecursiveInsertionSort<>();
 		String[] copy1 = {"Chorong", "Bomi", "Eunji", "Hayoung", "Naeun", "Namjoo"};
 
 		System.out.println(Arrays.toString(vetorStrings));
@@ -155,6 +155,20 @@ public class StudentSortingTest {
 		System.out.println(Arrays.toString(vetorStrings));
 
 		Assert.assertArrayEquals(copy1, vetorStrings);
+	}
+	
+	@Test
+	public void testSort10() {
+		AbstractSorting<Integer> implementation2;
+		implementation2 = new RecursiveInsertionSort<>();
+		Integer[] arrayInt = {7, 6, 5, 4, 3, 2, 1};
+		Integer[] result = {7, 2, 3, 4, 5, 6, 1};
+
+		System.out.println(Arrays.toString(arrayInt));
+		implementation2.sort(arrayInt, 1, 5);
+		System.out.println(Arrays.toString(arrayInt));
+		
+		Assert.assertArrayEquals(result, arrayInt);
 	}
 
 	// MÉTODOS QUE OS ALUNOS PODEM CRIAR
