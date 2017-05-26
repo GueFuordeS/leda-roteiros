6c6d0137e09a10acebb91f8322e60a9af040c9b0
gabrielfernandesufcg <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-05-26 17:30:02 -0300

    r2

---
+++ R02-03/src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -0,0 +1,29 @@
+package sorting.divideAndConquer;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * Quicksort is based on the divide-and-conquer paradigm. The algorithm chooses
+ * a pivot element and rearranges the elements of the interval in such a way
+ * that all elements lesser than the pivot go to the left part of the array and
+ * all elements greater than the pivot, go to the right part of the array. Then
+ * it recursively sorts the left and the right parts. Notice that if the list
+ * has length == 1, it is already sorted.
+ */
+public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		
+	}
+	
+	private void reOrdArray(T[] array, int leftIndex, int pivot, int pointer, int rightIndex) {
+		if (pointer < rightIndex) {
+			if (array[pivot].compareTo(array[pointer]) > 0) {
+				Util.swap(array, pivot, pivot+1);
+				reOrdArray(array, leftIndex, pivot, pointer+1, rightIndex);
+			}
+		}
+	}
+}
