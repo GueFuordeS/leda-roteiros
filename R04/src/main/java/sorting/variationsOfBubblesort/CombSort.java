4bfab3ee40bf4b4a4581a9082bd5cdb58dd60798
Gabriel Fernandes <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-06-03 16:37:21 -0300

    CombSort

--- R04/src/main/java/sorting/variationsOfBubblesort/CombSort.java
+++ R04/src/main/java/sorting/variationsOfBubblesort/CombSort.java
@@ -1,14 +1,46 @@
 package sorting.variationsOfBubblesort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * The combsort algoritm.
  */
 public class CombSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if (array != null && leftIndex < rightIndex && leftIndex >= 0 && rightIndex - leftIndex >= 1
+	            && rightIndex < array.length) {
+			
+			int gap =  rightIndex - leftIndex;
+			double fator = 1.25;
+			boolean sorted = false;
+			
+			while (sorted == false) {
+				gap = (int) Math.floor(gap/fator);
+				
+				if (gap > 1) {
+					sorted = false;
+				}
+				else {
+					gap = 1;
+					sorted = true;
+				}
+				
+				int i = leftIndex;
+				while (i + gap <= rightIndex - leftIndex) {
+					if (array[i].compareTo(array[i+gap]) > 0) {
+						Util.swap(array, i, i+gap);
+						sorted = false;
+					}
+					i++;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(3/1.25);
 	}
 }
