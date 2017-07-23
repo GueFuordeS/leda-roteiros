d0fa50874146a82264c1c27560a4920b5b8542c3
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-07-23 04:30:24 -0300

    complicado

--- R13/src/main/java/adt/avltree/AVLTreeImpl.java
+++ R13/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -30,16 +30,48 @@ public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements
 
 	// AUXILIARY
 	protected void rebalance(BSTNode<T> node) {
-		int balance = this.calculateBalance(node);
-
-		if (balance > 1) {
-			if (this.calculateBalance((BSTNode<T>) node.getLeft()) < -1)
-				Util.leftRotation((BSTNode<T>) node.getLeft());
-			Util.rightRotation(node);
-		} else if (balance < -1) {
-			if (this.calculateBalance((BSTNode<T>) node.getRight()) > 1)
-				Util.rightRotation((BSTNode<T>) node.getRight());
-			Util.leftRotation(node);
+		if (node != null && !node.isEmpty()) {
+			boolean isRoot = false;
+			if (node.getParent().isEmpty()) {
+				isRoot = true;
+			}
+			int balance = this.calculateBalance(node);
+	
+			if (balance > 1) {
+				if (super.height((BSTNode<T>)node.getLeft().getLeft()) > -1
+						|| super.height((BSTNode<T>)node.getLeft().getRight()) > 0) {
+					if (this.calculateBalance((BSTNode<T>) node.getLeft()) < -1)
+						Util.leftRotation((BSTNode<T>) node.getLeft());
+					if (isRoot) {
+						this.root = Util.rightRotation(node);
+					}
+					else {
+						if (this.isChildPerLeft(node)) {
+							node.getParent().setLeft(Util.rightRotation(node));
+						}
+						else {
+							node.getParent().setRight(Util.rightRotation(node));
+						}
+					}
+				}
+			} else if (balance < -1) {
+				if (super.height((BSTNode<T>)node.getRight().getRight()) > -1
+						|| super.height((BSTNode<T>)node.getRight().getLeft()) > 0) {
+					if (this.calculateBalance((BSTNode<T>) node.getRight()) > 1)
+						Util.rightRotation((BSTNode<T>) node.getRight());
+					if (isRoot) {
+						this.root = Util.leftRotation(node);
+					}
+					else {
+						if (this.isChildPerLeft(node)) {
+							node.getParent().setLeft(Util.leftRotation(node));
+						}
+						else {
+							node.getParent().setRight(Util.leftRotation(node));
+						}
+					}
+				}
+			}
 		}
 	}
 
@@ -49,5 +81,31 @@ public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements
 			this.rebalance(node);
 			this.rebalanceUp((BSTNode<T>) node.getParent());
 		}
+	}
+	
+	private boolean isChildPerLeft(BSTNode<T> node) {
+		boolean retorno = false;
+		if (node != null && !node.isEmpty()) {
+			BSTNode<T> parent = (BSTNode<T>) node.getParent();
+			if (parent.getLeft().equals(node)) {
+				retorno = true;
+			}
+		}
+		return retorno;
+	}
+
+	public static void main(String[] args) {
+		AVLTreeImpl<Integer> avl = new AVLTreeImpl<>();
+		avl.insert(5);
+		avl.insert(1);
+		avl.insert(2);
+		avl.insert(3);
+	}
+	
+	@Override
+	public void insert(T element) {
+		super.insert(element);
+		BSTNode<T> node = this.search(element);
+		this.rebalanceUp(node);
 	}
 }
